f168990f4076b44d6b84d5e997b57f6ba78086c5
Ilacoume <ilacoume@irontec.com>
2022-03-07 17:21:38 +0100

    Added try/catch blocks
    Never worry about your log message being the one throwing the exception!
    *Performance is not affected*

--- Inker/src/main/java/com/inlacou/inker/Inker.kt
+++ Inker/src/main/java/com/inlacou/inker/Inker.kt
@@ -1,6 +1,7 @@
 package com.inlacou.inker
 
 import android.util.Log
+import java.lang.Exception
 
 object Inker {
 
@@ -9,41 +10,30 @@ object Inker {
 	fun mix(color: Color) { palette.add(color) }
 	fun unMix(color: Color) { palette.remove(color) }
 
-	fun d(cb: () -> String?)   { val d = palette.mapNotNull { it.d }; if(d.isNotEmpty()) cb().let { d.forEach { d -> d(it) } } }
+	fun d(cb: () -> String?)   { val d = palette.mapNotNull { it.d }; if(d.isNotEmpty()) try { cb().let { d.forEach { d -> d(it) } } }catch (ex: Exception) { e { ex } } }
 	fun w(cb: () -> Any?)   {
 		val w = palette.mapNotNull { it.w }
 		val w2 = palette.mapNotNull { it.w2 }
-		if(w.isNotEmpty() || w2.isNotEmpty()) cb().let {
+		if(w.isNotEmpty() || w2.isNotEmpty()) try { cb().let {
 		when (it) {
 			is Throwable -> w2.forEach { w2 -> w2(it) }
 			is String -> w.forEach { w -> w(it) }
 			else -> w.forEach { w -> w(it.toString()) }
 		}
-	} }
-	fun i(cb: () -> String?)   { val i = palette.mapNotNull { it.d }; if(i.isNotEmpty()) cb().let { i.forEach { i -> i(it) } } }
-	fun v(cb: () -> String?)   { val v = palette.mapNotNull { it.v }; if(v.isNotEmpty()) cb().let { v.forEach { v -> v(it) } } }
+	} }catch (ex: Exception) { e { ex } } }
+	fun i(cb: () -> String?)   { val i = palette.mapNotNull { it.d }; if(i.isNotEmpty()) try { cb().let { i.forEach { i -> i(it) } } }catch (ex: Exception) { e { ex } } }
+	fun v(cb: () -> String?)   { val v = palette.mapNotNull { it.v }; if(v.isNotEmpty()) try { cb().let { v.forEach { v -> v(it) } } }catch (ex: Exception) { e { ex } } }
 	fun e(cb: () -> Any?)      {
 		val e = palette.mapNotNull { it.e }
 		val e2 = palette.mapNotNull { it.e2 }
-		if(e.isNotEmpty() || e2.isNotEmpty()) cb().let {
+		if(e.isNotEmpty() || e2.isNotEmpty()) try { cb().let {
 		when (it) {
 			is Throwable -> e2.forEach { e2 -> e2(it) }
 			is String -> e.forEach { e -> e(it) }
 			else -> e.forEach { e -> e(it.toString()) }
 		}
-	} }
-	fun wtf(cb: () -> String?) { val wtf = palette.mapNotNull { it.wtf }; if(wtf.isNotEmpty()) cb().let { wtf.forEach { wtf -> wtf(it) } } }
-
-	open class DebugColor: Color() {
-		override val v: ((s: String?) -> Unit)? = { Log.v(createTag(), it.toString()) }
-		override val d: ((s: String?) -> Unit)? = { Log.d(createTag(), it.toString()) }
-		override val i: ((s: String?) -> Unit)? = { Log.i(createTag(), it.toString()) }
-		override val w: ((s: String?) -> Unit)? = { Log.w(createTag(), it.toString()) }
-		override val w2: ((t: Throwable?) -> Unit)? = { Log.w(createTag(), it) }
-		override val e: ((s: String?) -> Unit)? = { Log.e(createTag(), it.toString()) }
-		override val e2: ((t: Throwable?) -> Unit)? = { Log.e(createTag(), null, it) }
-		override val wtf: ((s: String?) -> Unit)? =  { Log.wtf(createTag(), it) }
-	}
+	} }catch (ex: Exception) { e { ex } } }
+	fun wtf(cb: () -> String?) { val wtf = palette.mapNotNull { it.wtf }; if(wtf.isNotEmpty()) try { cb().let { wtf.forEach { wtf -> wtf(it) } } }catch (ex: Exception) { e { ex } } }
 
 	abstract class Color {
 		open val v: ((s: String?) -> Unit)? = null
@@ -62,6 +52,17 @@ object Inker {
 		}
 
 		private fun StackTraceElement.elementToTag(): String = className.substringAfterLast('.')
+	}
+
+	open class DebugColor: Color() {
+		override val v: ((s: String?) -> Unit)? = { Log.v(createTag(), it.toString()) }
+		override val d: ((s: String?) -> Unit)? = { Log.d(createTag(), it.toString()) }
+		override val i: ((s: String?) -> Unit)? = { Log.i(createTag(), it.toString()) }
+		override val w: ((s: String?) -> Unit)? = { Log.w(createTag(), it.toString()) }
+		override val w2: ((t: Throwable?) -> Unit)? = { Log.w(createTag(), it) }
+		override val e: ((s: String?) -> Unit)? = { Log.e(createTag(), it.toString()) }
+		override val e2: ((t: Throwable?) -> Unit)? = { Log.e(createTag(), null, it) }
+		override val wtf: ((s: String?) -> Unit)? =  { Log.wtf(createTag(), it) }
 	}
 
 }
--- app/src/main/java/com/inlacou/loggerlibraryproject/TimberReleaseTree.kt
+++ app/src/main/java/com/inlacou/loggerlibraryproject/TimberReleaseTree.kt
@@ -6,9 +6,7 @@ import timber.log.Timber
  * Timber tree that disables Timber.d messages
  */
 class TimberReleaseTree: Timber.DebugTree() {
-
 	override fun d(t: Throwable?) {}
 	override fun d(message: String?, vararg args: Any?) {}
 	override fun d(t: Throwable?, message: String?, vararg args: Any?) {}
-
 }
